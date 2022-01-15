41691a5c576052b7831d8aa40d2ac0664f023055
Shashank nayak <shashanknayak.1209@gmail.com>
2022-01-15 13:14:37 +0530

    css modification and responsive

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -4,59 +4,58 @@ const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 function Weather({ searchText, onClick, onChange, name, country, currentIndex, daily, setSelectedIndex }) {
-    console.log(daily.main)
+
 
     return (
-        <div className='card '>
+        <div className='card-wrapper '>
             <div className='search-sec'>
                 <input
-                    className='form-control'
+                    className='input-sec'
                     value={searchText}
                     onChange={onChange}>
 
                 </input>
                 <button
-                    className='btn btn-primary mt-2 ps-5 pe-5'
+                    className='btn btn-primary mt-2 mb-4 ps-5 pe-5'
                     onClick={onClick}>
                     search
                 </button>
             </div>
 
-            <div className='d-flex'>
-                <div className={`current-weather-card d-flex ${daily && daily.length > 0 && daily[currentIndex].weather[0].icon.endsWith('d') ? 'dbg' : 'nbg'}`} >
-                    <div className='flex-grow-1'>
-                        <div>
-                            <h3>
-                                {daily && daily.length > 0 &&
+
+            <div className={`current-weather-card  ${daily && daily.length > 0 && daily[currentIndex].weather[0].icon.endsWith('d') ? 'dbg' : 'nbg'}`} >
+                <div className='flex-grow-1 weather-report'>
+
+                    <h1>{name},{country}</h1>
+                    <h5>Humidity:{daily && daily.length > 0 && daily[currentIndex].humidity}</h5>
+                    <h5>Pressure:{daily && daily.length > 0 && daily[currentIndex].pressure}</h5>
+                    <h5>Temparature:{daily && daily.length > 0 && daily[currentIndex].temp.day}</h5>
+                </div>
+
+                <div className='img-desc-responsive'>
+                    <h3>
+                        {daily && daily.length > 0 &&
 
 
-                                    <img className='weatherCurrentImg'
-                                        alt={daily[currentIndex].weather[0].description}
-                                        src={`http://openweathermap.org/img/wn/${daily[currentIndex].weather[0].icon}@2x.png`}>
+                            <img className='weatherCurrentImg'
+                                alt={daily[currentIndex].weather[0].description}
+                                src={`http://openweathermap.org/img/wn/${daily[currentIndex].weather[0].icon}@2x.png`}>
 
-                                    </img>
-                                }
-                            </h3>
-                            <h3>{daily && daily.length > 0 && daily[currentIndex].weather[0].description}</h3>
-                        </div>
+                            </img>
+                        }
+                    </h3>
+                    <h3>{daily && daily.length > 0 && daily[currentIndex].weather[0].description}</h3>
 
-
-
-                    </div>
-                    <div >
-                        <h1>{name},{country}</h1>
-                        <h5>Humidity:{daily && daily.length > 0 && daily[currentIndex].humidity}</h5>
-                        <h5>Pressure:{daily && daily.length > 0 && daily[currentIndex].pressure}</h5>
-                        <h5>Temparature:{daily && daily.length > 0 && daily[currentIndex].temp.day}</h5>
-
-                    </div>
 
                 </div>
 
 
             </div>
 
-            <div className='d-flex'>
+
+
+
+            <div className=' week-sec'>
                 {daily.map((each, index) => {
                     const date = new Date(each.dt * 1000)
                     const day = days[date.getDay()]
